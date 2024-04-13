dbbda6369b02f1630f3bdfbfd0dfe697d9f23ead
NTUI RAOUL <134698011+ntuiraoul1@users.noreply.github.com>
2024-04-13 00:23:28 -0400

    Add files via upload

---
+++ forloops.py
@@ -0,0 +1,37 @@
+# # for loop is used for itarating over a sequence
+# # (which can be anything) 
+
+# mylist = [
+#     'C','Java','C#','C++','Dart','Py', 'Js',
+#     'php','Swift','objective-C']
+
+# # # Here we are looping through a list
+# for values in mylist:
+#     if values == 'Java':
+#         break
+#     break
+
+
+
+# 2D list andnested kkist in python
+
+
+my_list =[
+     [0,3,4],
+     [3,6,7],
+     [9,5,2]
+ ]
+print(my_list)
+
+
+
+# my_dictionary = {
+    # 'name':'raoul',
+    # 'school': 'university of buea',
+    # 'department':'software',
+    # 'level': 'level400',
+    # 'location': 'buea'
+# }
+
+# for values in my_dictionary:
+    # print(values)
